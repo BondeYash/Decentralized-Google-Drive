0660b476763d528ed2c020c6ff371968a66609d9
Yash Sanjay Bonde <103805387+BondeYash@users.noreply.github.com>
2023-03-28 20:08:53 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  },
+};require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+};
